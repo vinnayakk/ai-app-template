1fde5bd505de533961d94a3a38ac6bf2e389dc6e
Vinnayakk Bangarwaa <bangarwaa@gmail.com>
2026-09-22 14:42:19 +0530

    FastAPI testing

---
+++ tests/test_health.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+
+from ai_app_template.main import app
+
+client = TestClient(app)
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
